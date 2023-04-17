b650572c033d1909d3bac8917614a4c6edc3f855
anelson92 <115906480+anelson92@users.noreply.github.com>
2023-04-17 16:55:31 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,13 +20,6 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
